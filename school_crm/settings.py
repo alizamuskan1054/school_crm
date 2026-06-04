d26eaa2397263ac2e154dab23976932bdd37cc8e
alizamuskan1054 <alizamuskan1054@gmail.com>
2026-06-04 07:24:24 +0500

    settings static url changes

--- school_crm/settings.py
+++ school_crm/settings.py
@@ -162,10 +162,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-# STATIC_DIRS=[os.path.join(BASE_DIR,'static')]
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Media files configurations for user-uploaded files (e.g., profile pictures)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
